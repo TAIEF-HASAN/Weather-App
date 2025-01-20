c218184c1d70d0b76152636aaafdce457523174e
Taief Hasan <taief.hasan880@gmail.com>
2025-01-20 23:31:00 +0600

    Mod

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const error404 = document.querySelector('.not-found');
 
 search.addEventListener('click', () => {
 
-    const APIKey = 'pk.3c5e64489d3bb184299339b68dcb8da3';
+    const APIKey = '635b5cf7a522c63b5b57e2e846c9e8b9';
     const city = document.querySelector('.search-box input').value;
 
     if (city === '')
     return;
 
-    fetch(`https://api.locationiq.com/v1/autocomplete.php?key=pk.3c5e64489d3bb184299339b68dcb8da3&q=${query}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
     .then(response => response.json())
     .then(json => {
 
